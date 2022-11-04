575011ba724ca8a7faca3c17f62ad081d5711589
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2022-11-04 17:01:05 +0530

    first commit

---
+++ src/test/java/com/example/streamJava/StreamJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
